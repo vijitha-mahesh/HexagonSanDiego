785414a5990c0b496dceefc740f6275dd5fc77ce
Tharindu Kumarasinghe <57245404+TharinduK97@users.noreply.github.com>
2022-03-15 10:49:36 +0530

    Develop : FloorPlan Repository

---
+++ Repositories/Repository/FloorPlanRepository.cs
@@ -0,0 +1,20 @@
+﻿using HexagonSanDiego.Dtos.FloorPlanDtos;
+using HexagonSanDiego.Repositories.IRepository;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HexagonSanDiego.Repositories.Repository
+{
+    public class FloorPlanRepository : IFloorPlanRepository
+    {
+        public Task<IEnumerable<GetFloorPlanDto>> AddFloorPlan(AddFloorPlanDto newFloorPlan)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<GetFloorPlanDto>> GetFloorPlan()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
