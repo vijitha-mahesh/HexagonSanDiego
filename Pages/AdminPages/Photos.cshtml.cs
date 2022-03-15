8d9d0fb7ebcee5471492795580013eb86cf3a360
vijitha mahesh <185044c@uom.lk>
2022-03-16 00:05:18 +0530

    develop : admin dashboard first page

---
+++ Pages/AdminPages/Photos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HexagonSanDiego.Pages.AdminPages
+{
+    public class PhotosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/AdminPanel.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HexagonSanDiego.Pages
+{
+    public class AdminPanelModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
