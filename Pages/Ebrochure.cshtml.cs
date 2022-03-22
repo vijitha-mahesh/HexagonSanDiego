3f5538d74f38366d4274bd14cd26fd8a330e9bcc
vijitha mahesh <185044c@uom.lk>
2022-03-22 13:08:02 +0530

    Develop: data binding e Broucher

--- Pages/Ebrochure.cshtml.cs
+++ Pages/Ebrochure.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HexagonSanDiego.Dtos.FloorPlanDtos;
+using HexagonSanDiego.Repositories.IRepository;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,23 @@ namespace HexagonSanDiego.Pages
 {
     public class EbrochureModel : PageModel
     {
+        private readonly IFloorPlanRepository _floorPlanRepository;
+
+
+        [BindProperty]
+        public AddFloorPlanDto addFloorPlanDto { get; set; }
+
+        public List<GetFloorPlanDto> allFloorPlans = new List<GetFloorPlanDto>();
+
+
+        public EbrochureModel(IFloorPlanRepository floorPlanRepository)
+        {
+            _floorPlanRepository = floorPlanRepository;
+
+        }
         public void OnGet()
         {
+            allFloorPlans = _floorPlanRepository.GetFloorPlan().Result;
         }
     }
 }
